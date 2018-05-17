8e2a80318d670692ed07cf5862a367b9a2fd8bbf
smilytokyo <madongjing_love@163.com>
2018-05-17 10:59:44 +0800

    代码题1

---
+++ react-demo/src/HelloComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HelloComponent extends React.Component{
+    render(){
+      return (
+        <div>Hello World</div>
+      )
+    }
+}
+
+export default HelloComponent;
---
+++ react-demo/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloComponent from './HelloComponent';
+
+ReactDOM.render(<HelloComponent />, document.getElementById('root'));
+
